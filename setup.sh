935e719106793884609d96d9c5ccbed9333d6b6f
Souptik Datta <souptikdatta2001@gmail.com>
2022-04-20 00:37:12 +0530

    chore: Add error checks for folders presence

--- setup.sh
+++ setup.sh
@@ -11,10 +11,17 @@ fi
 
 # Check if the supplied path exixts - if not exit.
 
-if [[ ! -d "$2" ]]
+if [[ ! -d "$2/" ]]
 then
     echo "ERROR : $2 does not exists on your filesystem."
 	echo "Usage : setup.sh [name_of_the_project] [path/to/the/folder/to/setup/project] [(optional)ssh_or_https_link_to_clone_wp_content_repo]"
+	exit 128
+fi
+
+if [[ ! -d "~/wordpress/wordpress.setup-DO-NOT-DELETE/" ]]
+then
+	echo "ERROR : The folder ~/wordpress/wordpress.setup-DO-NOT-DELETE/ doesn't exists."
+	echo "Make sure that folder is present and all the nessesary components are present inside that as it will be used."
 	exit 128
 fi
 
@@ -40,6 +47,13 @@ project_folder="$2/$1"
 
 echo " Project folder is being created at location - $project_folder"
 mkdir "$project_folder"
+
+if [[ ! -d "$project_folder/" ]]
+then
+    echo "ERROR : Project folder( $project_folder ) not found."
+	echo "Somehow the folder didn't get created. Do you see any other errors above?"
+	exit 128
+fi
 
 # Create copy the `.lando.yml`, `wp-config.php`, `.lando` to that folder.
 
