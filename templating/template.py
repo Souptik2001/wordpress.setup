9c6c587aa4d21acb483f0e2b6346db419d3ad727
Souptik Datta <souptikdatta2001@gmail.com>
2022-10-08 14:38:03 +0530

    chore: Add support for subdomain multisite install

--- templating/template.py
+++ templating/template.py
@@ -12,6 +12,8 @@ parser.add_argument("--appName", help="Website/application name",required=True)
 parser.add_argument("--php", help="PHP version", default=defaultPHP)
 parser.add_argument("--wp", help="WordPress version", default=defaultWP)
 parser.add_argument("--node", help="Node version", default=defaultNode)
+parser.add_argument("--multisite", help="Multisite or not", default=defaultNode, required=True, choices=['yes', 'no'])
+parser.add_argument("--multisiteType", help="Multisite type", default=defaultNode, choices=['subdomain', 'subdirectory'])
 
 args=parser.parse_args()
 
@@ -25,12 +27,19 @@ environment = Environment(loader=FileSystemLoader("templates/"))
 # Lando config file
 landoConfigTemplate = environment.get_template(".lando.yml.j2")
 
+
+coreInstallCommand = "wp core multisite-install --url=https://$LANDO_APP_NAME.$LANDO_DOMAIN --title=$LANDO_APP_NAME --admin_user=admin --admin_password=admin --admin_email=admin@souptik.dev --skip-email --skip-config"
+
+if args.multisite and args.multisite == "no":
+	coreInstallCommand = "wp core install --url=https://$LANDO_APP_NAME.$LANDO_DOMAIN --title=$LANDO_APP_NAME --admin_user=admin --admin_password=admin --admin_email=admin@souptik.dev --skip-email"
+
 filename = f"./tmp/.lando.yml"
 landoConfigContent = landoConfigTemplate.render(
 	app=f"{args.appName}",
 	php=f"{phpVer}",
 	wp=f"{wpVer}",
-	node=f"{nodeVer}"
+	node=f"{nodeVer}",
+	core_install_command=coreInstallCommand
 )
 with open(filename, mode="w", encoding="utf-8") as message:
 	message.write(landoConfigContent)
@@ -39,8 +48,36 @@ with open(filename, mode="w", encoding="utf-8") as message:
 # WordPress config file
 wpConfigTemplate = environment.get_template("wp-config.php.j2")
 
+
+multiSiteConf = f"""
+define('WP_ALLOW_MULTISITE', true);
+define( 'MULTISITE', true );
+define( 'DOMAIN_CURRENT_SITE', '{args.appName}.lndo.site' );
+define( 'PATH_CURRENT_SITE', '/' );
+define( 'SITE_ID_CURRENT_SITE', 1 );
+define( 'BLOG_ID_CURRENT_SITE', 1 );
+"""
+
+if args.multisiteType and args.multisiteType == "subdirectory":
+	multiSiteConf += "define( 'SUBDOMAIN_INSTALL', false );"
+else:
+	multiSiteConf += "define( 'SUBDOMAIN_INSTALL', true );"
+
+if args.multisite and args.multisite == "no":
+	multiSiteConf = ""
+	coreInstallCommand = """
+	wp core install --url=https://$LANDO_APP_NAME.$LANDO_DOMAIN \
+	--title=$LANDO_APP_NAME \
+	--admin_user=admin \
+	--admin_password=admin \
+	--admin_email=admin@souptik.dev \
+	--skip-email
+	"""
+
 filename = f"./tmp/wp-config.php"
-wpConfigContent = wpConfigTemplate.render()
+wpConfigContent = wpConfigTemplate.render(
+	multisite_conf=multiSiteConf
+)
 with open(filename, mode="w", encoding="utf-8") as message:
 	message.write(wpConfigContent)
 	print(f"Created: {filename}")
